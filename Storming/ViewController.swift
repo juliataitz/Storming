613bee3898f68052133ec12274d14d9dd23256f9
juliataitz <juliataitz@gmail.com>
2014-11-04 23:45:50 -0500

    First commit

---
+++ Storming/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Storming
+//
+//  Created by Julia Taitz on 10/26/14.
+//  Copyright (c) 2014 Julia Taitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let apiKey = "ae2fb2d6492878aaab8d12549caabbff"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+   
+        
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
+        
+        let weatherData = NSData(contentsOfURL: forecastURL!, options: nil, error: nil)
+        //prettyjson.com
+        
+        let sharedSession = NSURLSession.sharedSession()
+        let downloadTask: NSURLSessionDownloadTask =
+            sharedSession.downloadTaskWithURL(forecastURL!,
+            completionHandler: { (location: NSURL!, response:
+            NSURLResponse!, error: NSError!) -> Void in
+            
+            if (error == nil) {
+                let dataObject = NSData(contentsOfURL: location)
+                let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as NSDictionary
+                let currentWeather = Current(weatherDictionary: weatherDictionary)
+                println(currentWeather.currentTime!)
+                
+            }
+                
+        })
+        downloadTask.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
